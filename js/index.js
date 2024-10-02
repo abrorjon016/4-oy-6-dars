d8a23bc0a4a2c62bd139f2eef4a3414fc58d83bd
abrorjon016 <ikromjonovabrorjon3@gmail.com>
2024-10-03 01:18:11 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,126 @@
+// 1-masala
+// for (let i = 0; i <= 5; i++) {
+//   console.log(i);
+// }
+
+// 2-masala
+// for (let i = 0; i <= 20; i++) {
+//   console.log(i);
+// }
+
+// 3-masala
+
+// 4-masala
+// for (let i = 0; i <= 10; i += 2) {
+//   console.log(i);
+// }
+
+// 5-masala
+// for (let i = 1; i <= 15; i += 2) {
+//   console.log(i);
+// }
+
+// 6-masala
+// for (let i = 0; i <= 5; i++) {
+// let son = prompt("sonni kiriting");
+//   console.log(son);
+// }
+
+// 7-masala
+// for (let i = 15; i >= 5; i--) {
+//   console.log(i);
+// }
+
+// 8-masala
+// let num = prompt("sonni kiring");
+// for (let i = 0; i <= num ** 2; i++) {
+//   console.log(i);
+// }
+
+// 9-masala
+// let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//   let num = +prompt(`${i}-sonni kiriring`);
+//   sum += num;
+// }
+// console.log(sum);
+
+// 10-masala
+// for (let i = 1; i <= 50; i++) {
+//     if (i % 5 === 0) {
+//       console.log(i);
+//     }
+//   }
+
+// 11-masala
+// for (let i = 100; i >= 5; i--) {
+//   console.log(i);
+// }
+
+// 12 - masala
+// let num = 0;
+// for (let i = 1; i <= 100; i++) {
+//   num += i;
+// }
+// console.log(num);
+
+// 13-masala
+// let num1 = prompt("1-sonni kiriting");
+// let num2 = prompt("2-sonni kiriting");
+
+// for (let i = num1; i <= num2; i++) {
+//   console.log(i);
+// }
+
+// 14 - masala
+// let num = prompt("1-sonni kiriting");
+
+// for (let i = 1; i <= num; i++) {
+//   if (num % i == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 15-masala
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i * i);
+// }
+
+// 16-masala
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 7 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// 17-masala
+// tushundim lekin qande qilishni bilmadim
+//
+
+// 18-masala
+// let num = prompt("1-sonni kiriting:");
+// let sum = 0;
+
+// for (let i = 1; i <= num; i++) {
+//   if (num % i == 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+// shunga chatgpt ishlatdim )
+
+// 19 - masala
+// let sum = 0;
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 2 !== 0) {
+//     sum += i;
+//   }
+// }
+
+// console.log(sum);
+
+// 20-masala
+// 21-masala
+// 22, 23, 24, 25;
+// ???
